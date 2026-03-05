a36973ddd6681564f5dff296687619b2381f3315
Yuri Sulyma <yuri.sulyma@protonmail.com>
2026-03-05 19:59:18 +0100

    format html

--- graph-audiation/scene.js
+++ graph-audiation/scene.js
@@ -6,26 +6,29 @@ import { $, $e } from "/lib/utils.js";
 const container = document.getElementById("container");
 // create scene
 const { scene } = makeScene({
-    camera: {
-        position: [5.92, -2.05, 5.06],
-        up: [0, 0, 1], // math convention
-    },
-    container,
-    controls: OrbitControls,
+  camera: {
+    position: [5.92, -2.05, 5.06],
+    up: [0, 0, 1], // math convention
+  },
+  container,
+  controls: OrbitControls,
 });
 // add lights
 const ambientLight = new THREE.AmbientLight(undefined, 0.1);
 scene.add(ambientLight);
 const pointLights = [
-    { decay: 0, intensity: Math.PI, position: [0, 5, 5] },
-    { intensity: Math.PI, position: [0, 0, -2] },
+  { decay: 0, intensity: Math.PI, position: [0, 5, 5] },
+  { intensity: Math.PI, position: [0, 0, -2] },
 ];
 for (const config of pointLights) {
-    const pointLight = new THREE.PointLight(config.color, config.intensity, config.distance, config.decay);
-    // js syntax:
-    // fn(...[x, y, z]) is equivalent to fn(x, y, z)
-    pointLight.position.set(...config.position);
-    scene.add(pointLight);
+  const pointLight = new THREE.PointLight(
+    config.color,
+    config.intensity,
+    config.distance,
+    config.decay,
+  );
+  pointLight.position.set(...config.position);
+  scene.add(pointLight);
 }
 // axes helper
 scene.add(new THREE.AxesHelper(5));
@@ -33,183 +36,185 @@ const fn = (x, y) => Math.cos(2 * x) * Math.sin(y) + 1;
 let minZ, maxZ;
 // parametric surface
 {
-    const geometry = new ParametricGeometry((u, v, target) => {
-        const x = lerp(-5, 5, u);
-        const y = lerp(-5, 5, v);
-        const z = fn(x, y);
-        if (minZ === undefined) {
-            minZ = z;
-        }
-        else {
-            minZ = Math.min(z, minZ);
-        }
-        if (maxZ === undefined) {
-            maxZ = z;
-        }
-        else {
-            maxZ = Math.max(z, maxZ);
-        }
-        target.set(x, y, z);
-    }, 64, 64);
-    const material = new THREE.MeshPhongMaterial({
-        color: 0x00ff00,
-        opacity: 0.5,
-        side: THREE.DoubleSide,
-        transparent: true,
-    });
-    const surface = new THREE.Mesh(geometry, material);
-    scene.add(surface);
+  const geometry = new ParametricGeometry(
+    (u, v, target) => {
+      const x = lerp(-5, 5, u);
+      const y = lerp(-5, 5, v);
+      const z = fn(x, y);
+      if (minZ === undefined) {
+        minZ = z;
+      } else {
+        minZ = Math.min(z, minZ);
+      }
+      if (maxZ === undefined) {
+        maxZ = z;
+      } else {
+        maxZ = Math.max(z, maxZ);
+      }
+      target.set(x, y, z);
+    },
+    64,
+    64,
+  );
+  const material = new THREE.MeshPhongMaterial({
+    color: 0x00ff00,
+    opacity: 0.5,
+    side: THREE.DoubleSide,
+    transparent: true,
+  });
+  const surface = new THREE.Mesh(geometry, material);
+  scene.add(surface);
 }
 /**
  * input (x, y, 0) point
  */
 let input;
 {
-    const geometry = new THREE.SphereGeometry(0.1, 32, 32);
-    const material = new THREE.MeshPhongMaterial({
-        color: 0xff0000,
-        side: THREE.DoubleSide,
-    });
-    input = new THREE.Mesh(geometry, material);
-    scene.add(input);
+  const geometry = new THREE.SphereGeometry(0.1, 32, 32);
+  const material = new THREE.MeshPhongMaterial({
+    color: 0xff0000,
+    side: THREE.DoubleSide,
+  });
+  input = new THREE.Mesh(geometry, material);
+  scene.add(input);
 }
 /**
  * output (x, y, z) point
  */
 let output;
 {
-    const geometry = new THREE.SphereGeometry(0.1, 32, 32);
-    const material = new THREE.MeshPhongMaterial({
-        color: 0xff00ff,
-        side: THREE.DoubleSide,
-    });
-    output = new THREE.Mesh(geometry, material);
-    setOutputPosition();
-    scene.add(output);
+  const geometry = new THREE.SphereGeometry(0.1, 32, 32);
+  const material = new THREE.MeshPhongMaterial({
+    color: 0xff00ff,
+    side: THREE.DoubleSide,
+  });
+  output = new THREE.Mesh(geometry, material);
+  setOutputPosition();
+  scene.add(output);
 }
 output;
 /** amount to move the point by */
 const step = 0.1;
 function setOutputPosition() {
-    const { x, y } = input.position;
-    output.position.set(x, y, fn(x, y));
+  const { x, y } = input.position;
+  output.position.set(x, y, fn(x, y));
 }
 // keyboard interaction
 document.body.addEventListener("keydown", (e) => {
-    console.log(e);
-    switch (e.key) {
-        case "ArrowLeft":
-            input.position.setX(input.position.x - step);
-            break;
-        case "ArrowRight":
-            input.position.setX(input.position.x + step);
-            break;
-        case "ArrowUp":
-            input.position.setY(input.position.y + step);
-            break;
-        case "ArrowDown":
-            input.position.setY(input.position.y - step);
-            break;
-        case "?": {
-            const controls = document.getElementById("controls");
-            if (!controls) {
-                throw new Error("could not find element with id 'controls'");
-            }
-            controls.classList.toggle("hidden");
-            break;
-        }
-        case " ":
-            playPitch(output.position.z);
-            break;
-        case ";": {
-            let { x, y, z } = output.position;
-            [x, y, z] = [x, y, z].map((t) => truncate(t));
-            speak(`x: ${x}, y: ${y}, z: ${z}`);
-            break;
-        }
+  console.log(e);
+  switch (e.key) {
+    case "ArrowLeft":
+      input.position.setX(input.position.x - step);
+      break;
+    case "ArrowRight":
+      input.position.setX(input.position.x + step);
+      break;
+    case "ArrowUp":
+      input.position.setY(input.position.y + step);
+      break;
+    case "ArrowDown":
+      input.position.setY(input.position.y - step);
+      break;
+    case "?": {
+      const controls = document.getElementById("controls");
+      if (!controls) {
+        throw new Error("could not find element with id 'controls'");
+      }
+      controls.classList.toggle("hidden");
+      break;
     }
-    setOutputPosition();
+    case " ":
+      playPitch(output.position.z);
+      break;
+    case ";": {
+      let { x, y, z } = output.position;
+      [x, y, z] = [x, y, z].map((t) => truncate(t));
+      speak(`x: ${x}, y: ${y}, z: ${z}`);
+      break;
+    }
+  }
+  setOutputPosition();
 });
 // MDN voice example
 const synth = window.speechSynthesis;
 const voiceSelect = $("select");
 let voices = [];
 function populateVoiceList() {
-    voices = synth
-        .getVoices()
-        .filter((voice) => voice.lang.startsWith("en-"))
-        .sort((a, b) => {
-        const aname = a.name.toUpperCase();
-        const bname = b.name.toUpperCase();
-        if (aname < bname) {
-            return -1;
-        }
-        else if (aname === bname) {
-            return 0;
-        }
-        else {
-            return +1;
-        }
+  voices = synth
+    .getVoices()
+    .filter((voice) => voice.lang.startsWith("en-"))
+    .sort((a, b) => {
+      const aname = a.name.toUpperCase();
+      const bname = b.name.toUpperCase();
+      if (aname < bname) {
+        return -1;
+      } else if (aname === bname) {
+        return 0;
+      } else {
+        return +1;
+      }
     });
-    const selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
-    voiceSelect.innerHTML = "";
-    for (const voice of voices) {
-        const option = $e("option");
-        option.textContent = `${voice.name} (${voice.lang})`;
-        if (voice.default) {
-            option.textContent += " -- DEFAULT";
-        }
-        option.setAttribute("data-lang", voice.lang);
-        option.setAttribute("data-name", voice.name);
-        voiceSelect.appendChild(option);
+  const selectedIndex =
+    voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = "";
+  for (const voice of voices) {
+    const option = $e("option");
+    option.textContent = `${voice.name} (${voice.lang})`;
+    if (voice.default) {
+      option.textContent += " -- DEFAULT";
     }
-    voiceSelect.selectedIndex = selectedIndex;
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
+  }
+  voiceSelect.selectedIndex = selectedIndex;
 }
 populateVoiceList();
 if (speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = populateVoiceList;
+  speechSynthesis.onvoiceschanged = populateVoiceList;
 }
 function speak(text) {
-    if (synth.speaking) {
-        console.error("speechSynthesis.speaking");
-        return;
-    }
-    if (text === "")
-        return;
-    const utterThis = new SpeechSynthesisUtterance(text);
-    utterThis.onend = () => {
-        console.log("SpeechSynthesisUtterance.onend");
-    };
-    utterThis.onerror = () => {
-        console.error("SpeechSynthesisUtterance.onerror");
-    };
-    const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
-    utterThis.voice = voices.find((voice) => voice.name === selectedOption);
-    // utterThis.pitch = pitch.value;
-    // utterThis.rate = rate.value;
-    synth.speak(utterThis);
+  if (synth.speaking) {
+    console.error("speechSynthesis.speaking");
+    return;
+  }
+  if (text === "") return;
+  const utterThis = new SpeechSynthesisUtterance(text);
+  utterThis.onend = () => {
+    console.log("SpeechSynthesisUtterance.onend");
+  };
+  utterThis.onerror = () => {
+    console.error("SpeechSynthesisUtterance.onerror");
+  };
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute("data-name");
+  utterThis.voice = voices.find((voice) => voice.name === selectedOption);
+  // utterThis.pitch = pitch.value;
+  // utterThis.rate = rate.value;
+  synth.speak(utterThis);
 }
 // pitches
 // https://github.com/ysulyma/stem-course/blob/main/content/framework/app/audio-graph-3d/client.tsx
-const audioCtx = new // biome-ignore lint/suspicious/noExplicitAny: safari compatibility
- (window.AudioContext || window.webkitAudioContext)();
+const audioCtx =
+  new // biome-ignore lint/suspicious/noExplicitAny: safari compatibility
+  (window.AudioContext || window.webkitAudioContext)();
 function playPitch(z) {
-    const oscillator = audioCtx.createOscillator();
-    const gainNode = audioCtx.createGain();
-    const duration = 0.5;
-    // remap pitch to [0, 1]
-    const pitch = (z - minZ) / (maxZ - minZ);
-    // Frequency range: C4 to A5
-    const minFreq = 261.63;
-    const maxFreq = 880;
-    const frequency = lerp(minFreq, maxFreq, pitch);
-    oscillator.type = "sine"; // try "square", "sawtooth", etc.
-    oscillator.frequency.value = frequency;
-    gainNode.gain.value = 0.2; // keep it gentle
-    oscillator.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
-    oscillator.start();
-    oscillator.stop(audioCtx.currentTime + duration);
+  const oscillator = audioCtx.createOscillator();
+  const gainNode = audioCtx.createGain();
+  const duration = 0.5;
+  // remap pitch to [0, 1]
+  const pitch = (z - minZ) / (maxZ - minZ);
+  // Frequency range: C4 to A5
+  const minFreq = 261.63;
+  const maxFreq = 880;
+  const frequency = lerp(minFreq, maxFreq, pitch);
+  oscillator.type = "sine"; // try "square", "sawtooth", etc.
+  oscillator.frequency.value = frequency;
+  gainNode.gain.value = 0.2; // keep it gentle
+  oscillator.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+  oscillator.start();
+  oscillator.stop(audioCtx.currentTime + duration);
 }
 //# sourceMappingURL=scene.js.map
 //# sourceMappingURL=scene.js.map
---
+++ mobius/script.ts
@@ -0,0 +1,264 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { ParametricGeometry } from "three/addons/geometries/ParametricGeometry.js";
+
+import { makeMeshDraggable, screenToScene } from "./three-utils.js";
+
+import { makeScene, type Pt3 } from "/lib/three-utils.js";
+
+const mobiusRadius = 2;
+
+const { cos, sin, PI } = Math,
+  TWOPI = 2 * PI;
+
+/** objects in our scene */
+const objects: {
+  /** the movable point in the base space S¹ */
+  ball: THREE.Mesh;
+
+  /** the fiber over the point in the base space */
+  fiber: THREE.Mesh;
+
+  /** the surface of the Möbius strip */
+  mobius: THREE.Mesh;
+  // biome-ignore lint/suspicious/noExplicitAny: these will get set below
+} = {} as any;
+
+const container = document.getElementById("container") as HTMLDivElement;
+
+// create scene
+const { camera, controls, renderer, scene } = makeScene({
+  camera: {
+    position: [3, 0.83, 2.15],
+    up: [0, 0, 1], // math convention
+  },
+  container,
+  controls: OrbitControls,
+});
+
+// add lights
+const ambientLight = new THREE.AmbientLight(undefined, 0.1);
+scene.add(ambientLight);
+
+type LightConfig = {
+  color?: THREE.Color;
+  decay?: number;
+  distance?: number;
+  intensity?: number;
+  position: Pt3;
+};
+
+const pointLights: LightConfig[] = [
+  { decay: 0, intensity: Math.PI, position: [0, 200, 0] },
+  { decay: 0, intensity: Math.PI, position: [100, 200, 100] },
+  { decay: 0, intensity: Math.PI, position: [-100, -200, -100] },
+];
+
+for (const config of pointLights) {
+  const pointLight = new THREE.PointLight(
+    config.color,
+    config.intensity,
+    config.distance,
+    config.decay,
+  );
+
+  pointLight.position.set(...config.position);
+  scene.add(pointLight);
+}
+
+/* populate the scene */
+const elevation = 1.5;
+
+const fiberColor = 0x1bbb68;
+const baseColor = 0x1a69b5;
+const totalColor = 0xffff00;
+const sectionColor = 0xff7000;
+
+// axes helper
+scene.add(new THREE.AxesHelper(5));
+
+// base circle
+{
+  class CustomCurve extends THREE.Curve<THREE.Vector3> {
+    constructor() {
+      super();
+    }
+
+    override getPoint(t: number, target = new THREE.Vector3()) {
+      return target.set(...mobius(t, 0.5));
+    }
+  }
+
+  const geometry = new THREE.TubeGeometry(
+    new CustomCurve(),
+    50,
+    0.02,
+    50,
+    true,
+  );
+  const material = new THREE.MeshBasicMaterial({ color: baseColor });
+  const line = new THREE.Mesh(geometry, material);
+  scene.add(line);
+}
+
+// equatorial circle
+{
+  class CustomCurve extends THREE.Curve<THREE.Vector3> {
+    constructor() {
+      super();
+    }
+
+    override getPoint(t: number, target = new THREE.Vector3()) {
+      return target.set(...mobius(t, 0.5));
+    }
+  }
+
+  const geometry = new THREE.TubeGeometry(
+    new CustomCurve(),
+    50,
+    0.02,
+    50,
+    true,
+  );
+  const material = new THREE.MeshBasicMaterial({
+    color: baseColor,
+    opacity: 0.5,
+    transparent: true,
+  });
+  const line = new THREE.Mesh(geometry, material);
+  line.position.z = elevation;
+  scene.add(line);
+}
+
+// ball control
+{
+  const geometry = new THREE.SphereGeometry(0.05, 20, 20);
+  const material = new THREE.MeshBasicMaterial({ color: baseColor });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.set(mobiusRadius, 0, 0);
+  scene.add(mesh);
+  objects.ball = mesh;
+}
+
+// mobius strip
+{
+  const geometry = new ParametricGeometry(
+    (theta, t, target) => {
+      target.set(...mobius(theta, t));
+    },
+    50,
+    50,
+  );
+  const material = new THREE.MeshPhongMaterial({
+    color: totalColor,
+    side: THREE.DoubleSide,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+  mesh.position.z = elevation;
+  objects.mobius = mesh;
+}
+
+// fiber
+{
+  const geometry = new THREE.CylinderGeometry(0.02, 0.02, 1);
+  const material = new THREE.MeshBasicMaterial({ color: fiberColor });
+  const line = new THREE.Mesh(geometry, material);
+  line.rotateX(TWOPI / 4);
+  line.position.set(mobiusRadius, 0, elevation);
+  scene.add(line);
+  objects.fiber = line;
+}
+
+// section
+{
+  class CustomCurve extends THREE.Curve<THREE.Vector3> {
+    constructor() {
+      super();
+    }
+
+    override getPoint(t: number, target = new THREE.Vector3()) {
+      return target.set(...mobius(t, 0.5 - Math.sin(t * Math.PI) / 2.5));
+    }
+  }
+  const geometry = new THREE.TubeGeometry(
+    new CustomCurve(),
+    50,
+    0.02,
+    50,
+    true,
+  );
+  const material = new THREE.MeshBasicMaterial({ color: sectionColor });
+  const line = new THREE.Mesh(geometry, material);
+  line.position.z = elevation;
+  scene.add(line);
+}
+
+// drag functionality
+makeMeshDraggable(
+  objects.ball,
+  {
+    move: ({ x, y }) => {
+      const pos = screenToScene(
+        x,
+        y,
+        new THREE.Plane(new THREE.Vector3(0, 0, 1)),
+        renderer,
+        camera,
+      );
+
+      const theta = (Math.atan2(pos.y, pos.x) + TWOPI) % TWOPI;
+
+      objects.ball.position.set(
+        mobiusRadius * cos(theta),
+        mobiusRadius * sin(theta),
+        0,
+      );
+
+      const angle = theta / TWOPI;
+      const [rotation, position] = arrowOrient(
+        new THREE.Vector3(...mobius(angle, 0)),
+        new THREE.Vector3(...mobius(angle, 1)),
+      );
+      position.z = 1.5;
+      objects.fiber.setRotationFromMatrix(rotation);
+      objects.fiber.position.copy(position);
+    },
+  },
+  {
+    camera,
+    container,
+    controls,
+    renderer,
+  },
+);
+
+function mobius(theta: number, t: number): Pt3 {
+  const R = mobiusRadius,
+    r = 0.5,
+    n = 1;
+
+  theta *= TWOPI;
+  t = -r + 2 * r * t;
+
+  return [
+    cos(theta) * (R - t * sin((theta * n) / 2)),
+    sin(theta) * (R - t * sin((theta * n) / 2)),
+    t * cos((theta * n) / 2),
+  ];
+}
+
+function arrowOrient(pointX: THREE.Vector3, pointY: THREE.Vector3) {
+  const orientation = new THREE.Matrix4();
+  orientation.lookAt(pointX, pointY, new THREE.Object3D().up);
+  orientation.multiply(
+    new THREE.Matrix4().set(1, 0, 0, 0, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0, 0, 1),
+  );
+
+  const position = new THREE.Vector3(
+    (pointY.x + pointX.x) / 2,
+    (pointY.y + pointX.y) / 2,
+    (pointY.z + pointX.z) / 2,
+  );
+  return [orientation, position] as [THREE.Matrix4, THREE.Vector3];
+}
